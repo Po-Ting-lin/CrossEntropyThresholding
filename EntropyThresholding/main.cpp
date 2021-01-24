8a2b12aa58d1b1ccacbf11d3f3807f51b30b228c
Po-Ting-lin <c1c4c5c6@gmail.com>
2021-01-24 15:28:23 +0800

    clean

--- EntropyThresholding/main.cpp
+++ EntropyThresholding/main.cpp
@@ -1,18 +1,5 @@
 #include "entropy_thresholding.h"
 
-static void displayImage(const cv::Mat& image, const char* name, bool mag) {
-	cv::Mat Out;
-	if (mag) {
-		cv::resize(image, Out, cv::Size(image.cols / 4, image.rows / 4), 5, 5);
-	}
-	else {
-		image.copyTo(Out);
-	}
-	namedWindow(name, cv::WINDOW_AUTOSIZE);
-	cv::imshow(name, Out);
-	cv::waitKey(0);
-}
-
 int main(void) {
 	cv::Mat src, dst;
 	src = cv::imread(R"(test.png)", cv::IMREAD_GRAYSCALE);
@@ -22,21 +9,13 @@ int main(void) {
 		return -1;
 	}
 
-	displayImage(src, "image", false);
-
-	/*******************************************************/
 	auto start = std::chrono::system_clock::now();
 	EntropyBasedThreshold entropy_based_threshold(src);
 	entropy_based_threshold.Process();
 	auto end = std::chrono::system_clock::now();
 	cv::threshold(src, dst, entropy_based_threshold.Process(), DYNAMICRANGE - 1, 0);
 
-	// time
 	std::chrono::duration<double> elapsed_seconds = end - start;
 	std::cout << "total time consume: " << elapsed_seconds.count() << std::endl;
-	/*******************************************************/
-
-	displayImage(dst, "output", false);
-
 	return 0;
 }
